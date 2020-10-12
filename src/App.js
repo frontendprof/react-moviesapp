ddd810325aedba7faed95d2453782c6bc8c9fb7f
frontendprof <abdumaliksharipov@gmail.com>
2020-10-12 22:08:06 +0500

    Getting img data on screen

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Movie from "./components/Movie";
 
 const FEATURED_API="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6283d969a7fbafe2c3d550f35ed4060d&page=1";
 
-const IMG_API="https://image.tmdb.org/t/p/w1280";
+
 
 const SEARCH_API="https://api.themoviedb.org/3/search/movie?&api_key=6283d969a7fbafe2c3d550f35ed4060d&query=";
 
@@ -31,9 +31,9 @@ function App() {
 
 
   return (
-    <div className="App">
+    <div className="App" >
       {movies.length>0 && movies.map(m=>(
-        <Movie />
+        <Movie key={m.id} {...m}/>
       ))}
     </div>
   );
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,11 +3,18 @@ import React from 'react';
 
 
 
+const IMG_API="https://image.tmdb.org/t/p/w1280";
 
-const Movie=()=> {
+
+const Movie=({title,poster_path,overview,vote_average})=> {
     return (
         <div className="movie">
-            Some movie component here
+            <img src={IMG_API+poster_path} alt={title}/>
+            <div className="movie-info">
+                <h3>{title}</h3>
+                <span>{vote_average}</span>
+            </div>
+           
         </div>
     );
 }
