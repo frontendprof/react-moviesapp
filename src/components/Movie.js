1568a4048ea53b758aa974304501b554d9dfbe9c
frontendprof <abdumaliksharipov@gmail.com>
2020-10-13 23:56:20 +0500

    WOrking on rating info style

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,13 +6,25 @@ import React from 'react';
 const IMG_API="https://image.tmdb.org/t/p/w1280";
 
 
+const setVoteClass=(vote)=>{
+
+   if(vote>=8){
+       return "highlight"
+   }else if(vote>=6){
+       return "average"
+   }else{
+       return "no-highlight"
+   }
+}
+
+
 const Movie=({title,poster_path,overview,vote_average})=> {
     return (
         <div className="movie-cont">
             <img src={IMG_API+poster_path} alt={title}/>
             <div className="movie-info">
                 <h3>{title}</h3>
-                <span>{vote_average}</span>
+                <span className={`${setVoteClass(vote_average)}`}>{vote_average}</span>
             </div>
 
             <div className="movie-overview">
