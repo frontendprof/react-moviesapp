64bdc0e8f83e9a12523d42012466a76f7478eb7e
frontendprof <abdumaliksharipov@gmail.com>
2020-10-14 00:25:23 +0500

    Glossing over

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,6 +4,7 @@ import React from 'react';
 
 
 const IMG_API="https://image.tmdb.org/t/p/w1280";
+const UNSPLASH_DEFAULT_IMG="https://images.unsplash.com/photo-1542204637-e67bc7d41e48?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
 
 
 const setVoteClass=(vote)=>{
@@ -21,10 +22,10 @@ const setVoteClass=(vote)=>{
 const Movie=({title,poster_path,overview,vote_average})=> {
     return (
         <div className="movie-cont">
-            <img src={IMG_API+poster_path} alt={title}/>
+            <img src={poster_path?(IMG_API+poster_path):UNSPLASH_DEFAULT_IMG} alt={title}/>
             <div className="movie-info">
                 <h3>{title}</h3>
-                <span className={`${setVoteClass(vote_average)}`}>{vote_average}</span>
+                <span className={`tag ${setVoteClass(vote_average)}`}>{vote_average}</span>
             </div>
 
             <div className="movie-overview">
